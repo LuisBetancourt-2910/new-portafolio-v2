0506dec982b84721dbd6dc22bdd8324073e01490
Luis Betancourt <joseluisgarciabeta@gmail.com>
2025-12-08 10:01:43 -0600

    se agrega webkit para que jale en todos lados
    se agrega webkit para que jale en todos lados

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,15 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
+    autoprefixer: {
+      overrideBrowserslist: [
+        "> 1%",
+        "last 2 versions",
+        "Firefox ESR",
+        "not dead",
+        "not ie 11"
+      ]
+    },
   },
 };
 
